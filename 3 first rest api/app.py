55f6dc000369bb72296843ab0928805388558121
ponjagannath <ponjagannath@gmail.com>
2022-12-07 22:08:08 +0530

    add

---
+++ 3 first rest api/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [
+  {
+    "name": "My Store",
+    "items": [
+      {
+        "name": "Chair",
+        "price": 15.99
+      }
+    ]
+  }
+]
+
+@app.get("/store") # localhost:5000/store
+def get_store():
+  return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+  request_data = request.get_json()
+  new_store = {"name": request_data["name"], 
+               "items": []
+               }
+  stores.append(new_store)
+  return new_store, 201
+
+
